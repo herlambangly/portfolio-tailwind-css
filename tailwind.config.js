badfd1af8119596c2746d07251ada27f3210d86c
herlambangly <hyudhian@gmail.com>
2023-02-25 23:34:01 +0800

    portfolio tilwind css

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        dark: "#0f172a",
+        secondary: "#475569",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
