303905836d08efac8e4b7859bc6a74f5cce1a8c3
herlambangly <hyudhian@gmail.com>
2023-02-27 20:11:37 +0800

    Edit Config & HTML

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}", "./index.html"],
+  content: { relative: true, files: ["./public/**/*.{html,js}", "./index.html"] },
   theme: {
     container: {
       center: true,
